2b1075eab891fadb8e21068728b572e30ed39ea5
DavidZuluaga1991 <davidzuluaga1991@gmail.com>
2017-02-26 16:20:06 -0500

    revisando menu

--- probando/public_html/js/controller/ItemController.js
+++ probando/public_html/js/controller/ItemController.js
@@ -7,7 +7,7 @@
 
 app.controller('ItemController', ['$scope', function (scope) {
         scope.$parent.isopen = (scope.$parent.default === scope.item);
-        scope.$watch('isopen', function (newvalue, oldvalue, scope) {
+        /*scope.$watch('isopen', function (newvalue, oldvalue, scope) {
             scope.$parent.isopen = newvalue;
-        });
+        });*/
     }]);
