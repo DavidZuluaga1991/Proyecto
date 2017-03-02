ddec63f2b4e60810252afcfce9199955fb759dca
DavidZuluaga1991 <davidzuluaga1991@gmail.com>
2017-03-01 23:11:36 -0500

    Mejoras en el Contactenos

--- probando/public_html/js/controller/contact/contactcontroller.js
+++ probando/public_html/js/controller/contact/contactcontroller.js
@@ -1,10 +1,18 @@
 
 
-/* global contact, comment */
-contact.controller('contactcontroller', function() {
+/* global contact */
+contact.controller('contactcontroller', function () {
+    this.comments = [];
     this.clear = function () {
         this.comment.name = "";
-            this.comment.email = "";
-            this.comment.phone = "";
-          };
+        this.comment.email = "";
+        this.comment.phone = "";
+    };
+    
+    this.addcomments = function (){
+        alert("pro");
+       this.comment.date = Date.now();
+       this.comments.push(comment.body);
+    };
+    
 });
