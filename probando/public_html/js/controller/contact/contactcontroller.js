4d06d560da983a0c67c9cbd8bb217d414d3ffc42
SoftPymesZuluaga <produccion15@softpymes.com.co>
2017-03-02 17:33:04 -0500

    Mejoras en el proyecto para la parte de contactenos y el diseño.

--- probando/public_html/js/controller/contact/contactcontroller.js
+++ probando/public_html/js/controller/contact/contactcontroller.js
@@ -3,16 +3,27 @@
 /* global contact */
 contact.controller('contactcontroller', function () {
     this.comments = [];
+    this.comment = {};
     this.clear = function () {
-        this.comment.name = "";
-        this.comment.email = "";
-        this.comment.phone = "";
+        this.comment.name = null;
+        this.comment.email = null;
+        this.comment.phone = null;
+        this.comment.body = null;
+        this.contactform.$valid = false;
+    };
+    
+    this.ok = function () {
+        if(this.contactform.$invalid) {
+            alert ('Llenar formulario');
+        }else{
+            alert ('todo esta bien');
+        }
     };
     
     this.addcomments = function (){
-        alert("pro");
        this.comment.date = Date.now();
-       this.comments.push(comment.body);
+       this.comments.push(this.comment);
+       this.comment = {};
     };
     
 });
